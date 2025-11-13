a1ad21810f29ddbad193f135f7e1d96c33918c1a
vishva <vishumovaliya53@gmail.com>
2025-11-13 13:37:38 +0530

    Practice

--- python Exam/project6.py
+++ python Exam/project6.py
@@ -1,63 +1,84 @@
-print()
 
-file = "main.txt"
+print("\nWelcome To The Journal Manager!")
+
+import datetime
 
 class filedata:
     def __init__ (self,filename="main.txt"):
         self.filename=filename
-        
+
     def Add_Entry(self,entry):
         with open(self.filename,"a") as file:
-            file.write(entry + "\n")
-            print("Entry Added successfully !!")
+            now = datetime.datetime.now()
+            print(now)
+            file.write(entry + "\n") 
+            print("\nEntry Added successfully !!")
             
     def View_Entry(self):
         try:
             with open(self.filename,"r")as file:
                 v=file.read()
                 print("\n--- Journal Entries ---\n")
-
+                now = datetime.datetime.now()
+                print(now)
                 print(v)
         except FileNotFoundError:
-            print("File Not Found!")
+            print("\nNo Journal Entries found. Start by Adding New Entry!")
             
     def Search_Entry(self):
-        s=input("Enter A Keyword for Search: ")
+        s=input("\nEnter A Keyword for Search: ")
         with open("main.txt","r")as file:
-            a=file.readline()
-        for line in enumerate(a):
-            if s in line:
-                print(line)
+            a=file.readlines ()
+        for linenum, a in enumerate(a):
+            if s in a:
+                print(linenum , "     " , a)
+            else:
+                print("No Entries Were Found For The Keyword !!")
                 
     def Delete_Entry(self):
         with open (self.filename,"w") as file:
             file.write("")
-            print("All Journal Entries Deleted !")
+        verify = str(input("\nYou Want To Delete This File(y/n): "))
+        if verify == "y":
+            print("\nAll journal have been deleted!!")
+        elif verify == "n":
+            print("\nDone")
+        else:
+            print("\nNo journal Entries to Delete.") 
             
-jm=filedata("main.txt") 
+jm=filedata("main.txt")
 
 while True:
-    print("\n1. Add a New Entry")
-    print("2. View All Entries")
-    print("3. Search for an Entry")
-    print("4. Delete All Entries")
+
+    print("\n1. Add new entry")
+    print("2. View entries")
+    print("3. Search entry")
+    print("4. Delete entries")
     print("5. Exit")
 
-
-    choice = input("\nEnter your choice: ")
+    try:
+        choice = int(input("\nEnter your choice: "))
+    except ValueError:
+        print("\nPlease enter a valid number.")
+        continue
 
     if choice == 1:
-            entry = input("Write your journal entry: ")
-            jm.Add_Entry(entry)
+        entry = input("\nEnter Your Journal Entry: ")
+        jm.Add_Entry(entry)
+    
     elif choice == 2:
-            jm.View_Entry()
+        jm.View_Entry()
+    
     elif choice == 3:
-            keyword = input("Enter keyword/date to search: ")
-            jm.Search_Entry()
+        print("\nShow Matching Entries:\n")
+        jm.Search_Entry()
+    
     elif choice == 4:
-            jm.Delete_Entry()
+        jm.Delete_Entry()
+   
     elif choice == 5:
-            print("Exiting Journal App.")
-            break
-    else: 
-      print("INVALID CHOICE !!")
+        print("\nThank you for Using Personal Journal Manager. Goodbye!!\n")
+        break
+    
+    else:
+        print("\nInvalid Option. Please select a valid Option!")
